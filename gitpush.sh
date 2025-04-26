4c641992b920db26edf2631dbcafe92416d241d2
dltv19 <sultandaulatov@gmail.com>
2025-04-26 23:37:42 +0500

    .

--- gitpush.sh
+++ gitpush.sh
@@ -1,14 +1,14 @@
 # 1 пуш
-git init
-git add .
-git commit -m "Ois"
-git remote add origin git@github.com:daulyatov/ois2.git
-git push origin master
+# git init
+# git add .
+# git commit -m "Ois"
+# git remote add origin git@github.com:daulyatov/ois2.git
+# git push origin master
 
 # след пуши 
-# git add .
-# git commit -m "."
-# git push origin master
+git add .
+git commit -m "."
+git push origin master
 
 # при ошибках
 # git pull origin master --rebase
